d7373c090842ade8f39051a6f9c327d6ae5e7cad
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2022-08-30 15:51:31 +0400

    Add files via upload
    Premier calcul avec JS

---
+++ script.js
@@ -0,0 +1,20 @@
+
+let coteA = (Number);
+let coteB = (Number);
+
+
+//saisie des paramètres 
+alert("CALCUL HYPOTHENUSE D'UN TRIANGLE RECTANGLE");
+//affichage demande calcul
+coteA = prompt("Entrez la longueur du coté A du triangle"); //entrez la 1er valeur
+
+coteB = prompt("Entrez la longueur du coté B du triangle"); //*****2e valeur
+
+//calcul  
+let hypo = (coteA*coteA + coteB*coteB) //formule
+
+//affichage des résultats 
+alert("l'hypothenuse C vaut " + hypo);
+console.log("l'hypothenuse C vaut " + hypo);
+
+
